0fec3b7c4e2591fbf173d205a82dd20622223250
Ryan Simkoff <rsimkoff@gmail.com>
2021-07-13 22:31:47 -0400

    adding post functionality

--- controllers/api/blogPostRoutes.js
+++ controllers/api/blogPostRoutes.js
@@ -1,7 +1,84 @@
 const router = require("express").Router();
+const { Post } = require("../../models");
 
-router.use("/", async (req, res) => {
-  res.json({ message: "You hit route: /api/posts." });
+router.get("/", async (req, res) => {
+  try {
+    const postResults = await Post.findAll();
+    if (!postResults) {
+      res.status(404).json({ message: "No post data found" });
+    } else {
+      res.status(200).json(postResults);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
+router.get("/:id", async (req, res) => {
+  try {
+    const postResults = await Post.findAll(
+      { where: {id: req.params.id}}
+    );
+    if (!postResults) {
+      res.status(404).json({ message: "No post data found with that ID" });
+    } else {
+      res.status(200).json(postResults);
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+      const postResults = await Post.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+
+      res.status(200).json(postResults);
+    
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const postResults = await Post.update(
+      {
+        ...req.body,
+      },
+      {
+        where: { id: req.params.id },
+      }
+    );
+    res.status(200).json(postResults);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const postResults = await Post.destroy(
+      {
+        where: { id: req.params.id }
+      }
+    );
+
+    if (!postResults) {
+      res.status(404).json({ message :'No post found with that ID.'})
+      return;
+    }
+
+    res.status(200).json(postResults)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json(err)
+  }
+})
 
 module.exports = router;
